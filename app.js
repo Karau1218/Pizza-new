cdf74ca28754d46fc5a01a2fc75e52313f95f2f6
Fkarau <30944677+Karau1218@users.noreply.github.com>
2025-10-21 11:19:42 -0700

    added additional roots

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const PORT = 3000;
 // Enable static file serving
 app.use(express.static('public'));
 
+// install middlewear to parse form data (req.body)
+app.use(express.urlencoded({extend3ed: true}));
+
+// create an array to store orders
+const orders = [];
+
 //Define a default "route" ('/')
 //req: contains information about the incoming request
 //res: allows us to send back a response to the client
@@ -19,6 +25,53 @@ app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`)
 });
 
+//define "contact us" route
+app.get('/contact-us', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/contact.html`)
+
+})
+
+//define "confirmation" route
+app.get('/confirm', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
+
+})
+
+// for admin route to a new different page
+//define "admin" route
+app.get('/admin', (req, res) => {
+    res.send(orders);
+    //res.sendFile(`${import.meta.dirname}/views/admin.html`)
+
+})
+
+// for submit-order route to a new different page
+//define "admin" route
+app.post('/submit-order', (req, res) => {
+    console.log(req.body);
+    //res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
+
+// create a JSON object to store the data
+const order = {
+  firstname: req.body.firstname,
+  lastname: req.body.lastname,
+  email: req.body.email,
+  method: req.body.method,
+  topings: req.body.topings,  
+  size: req.body.size,
+  comment: req.body.comment, 
+};
+
+// Add order to array
+orders.push(order);
+console.log(orders);
+
+
+
+})
+
+
+// this is for it to show up on a different webiste
 //Start the server and make it listen to the port specified above
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
